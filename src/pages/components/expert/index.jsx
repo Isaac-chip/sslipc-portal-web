dd47db33dc07fa6a8063c9a368682c64dd2ad103
Isaac-chip <15622125865@163.com>
2023-04-06 16:45:46 +0800

    调整布局

--- src/pages/components/expert/index.jsx
+++ src/pages/components/expert/index.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Button from "./button";
 import ExpertCard from "./ExpertCard";
 export default function Expert() {
+  const count = 9;
   return (
     <div className="container mx-auto">
       <Image
@@ -18,17 +19,15 @@ export default function Expert() {
         <Button text={"技术专家"}></Button>
         <Button text={"知识产权专家"}></Button>
       </div>
-      <div className="mt-40px mb-131px flex flex-wrap sm:justify-between justify-center">
-        {
-            new Array(10).fill('').map((item)=>{
-               return (
-                <div className="mb-40px">
-                    <ExpertCard></ExpertCard>
-                </div>
-               )
-            })
-        }
-    
+      <div className="mt-40px mb-131px flex flex-wrap sm:justify-between justify-center flex-1">
+        {new Array(count).fill("").map((item) => {
+          return (
+            <div className="mb-40px">
+              <ExpertCard></ExpertCard>
+            </div>
+          );
+        })}
+       
       </div>
     </div>
   );
