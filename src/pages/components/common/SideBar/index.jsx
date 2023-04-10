240f9832026451cfd1826623f205990951a36e74
Isaac-chip <15622125865@163.com>
2023-04-10 14:59:23 +0800

    sidebar组件

--- src/pages/components/common/SideBar/index.jsx
+++ src/pages/components/common/SideBar/index.jsx
@@ -2,33 +2,16 @@
 import _ from "lodash"
 import { useEffect, useState } from "react";
 import classNames from "classnames";
-export default function SideBar() {
-  const [activity, setActivity] = useState([0]);
-  const [sideBarData, setSideBarData] = useState([
-    {
-      label: "维权流程",
-      children: [
-        { label: "维权援助流程" },
-        { label: "人民调解流程" },
-        { label: "仲裁调解流程" },
-        { label: "专利侵权纠纷提交材料" },
-      ],
-      open: true,
-    },
-    {
-      label: "全球维权援助网络",
-      children: [
-        { label: "知识产权保护中心" },
-        { label: "知识产权快速维权中心" },
-        { label: "知识产权维权援助中心" },
-      ],
-      open: false,
-    },
-  ]);
+export default function SideBar({sideBar}) {
+  const [activity, setActivity] = useState({
+    menuActivity:0,
+    subMenuActivity:0,
+    activity:[0,0]
+  });
 
-  const activityFn = (arr) => {
-    setActivity(arr);
-  };
+  const [sideBarData, setSideBarData] = useState(sideBar);
+
+
   const handleMenu = (index)=>{
     const data = _.cloneDeep(sideBarData)
     data[index].open=!data[index].open
@@ -42,30 +25,36 @@ export default function SideBar() {
           <div className="cursor-pointer">
             <div
               onClick={() => {
-                activityFn([index]),handleMenu(index)
+                handleMenu(index)
+               const data = _.cloneDeep(activity)
+               data.menuActivity=index
+               if(sideBarData[index].children.length==0){
+                    data.activity=[index]
+               }
+         
+               setActivity(data)
               }}
               className={classNames(
-                "h-72px w-210px flex justify-around items-center bg-white",
-                {}
+                "h-72px w-210px flex justify-around items-center bg-white"
               )}
               style={{
                 background:
-                  activity == [] || activity[0] === index
+                  activity.menuActivity === index
                     ? "url(/rightFlow/xuanzhong.png)"
                     : "",
               }}
             >
               <div
                 className={classNames("text-xl   hover:text-gray-200", {
-                  "text-white": activity[0] === index,
+                  "text-white":  activity.menuActivity=== index,
                 })}
               >
                 {item.label}
               </div>
               <img
-                className=""
+                className={classNames({"hidden":sideBar[index].children.length==0})}
                 src={
-                  activity[0] === index ? `/icon/shangla.png` : `/icon/down.png`
+                    activity.menuActivity === index ? `/icon/shangla.png` : `/icon/down.png`
                 }
                 alt=""
                 srcset=""
@@ -74,8 +63,15 @@ export default function SideBar() {
             {item?.children.map((item, subIndex) => {
               return (
                 <div
+                  onClick={()=>{
+                    const data = _.cloneDeep(activity)
+                    data.menuActivity=index
+                    data.subMenuActivity=subIndex
+                    data.activity=[index,subIndex]
+                    setActivity(data)
+                  }}
                   className={classNames(
-                    "h-40px bg-white w-210px flex justify-center items-center text-md hover:text-blue",{"hidden":!sideBarData[index].open}
+                    "h-40px bg-white w-210px flex justify-center items-center text-md hover:text-blue",{"hidden":!sideBarData[index].open},{"text-blue":activity.activity[0]==index && activity.activity[1]==subIndex}
                   )}
                 >
                   {item.label}
--- src/pages/components/rightsFlow/index.jsx
+++ src/pages/components/rightsFlow/index.jsx
@@ -3,6 +3,33 @@ import BreadcrumbPlus from "../common/BreadcrumbPlus";
 import SideBar from "../common/SideBar";
 
 export default function RightsFlow() {
+  const sideBarList = [
+    {
+      label: "维权流程",
+      children: [
+        { label: "维权援助流程" },
+        { label: "人民调解流程" },
+        { label: "仲裁调解流程" },
+        { label: "专利侵权纠纷提交材料" },
+      ],
+      open: true,
+    },
+    {
+      label: "全球维权援助网络",
+      children: [
+        { label: "知识产权保护中心" },
+        { label: "知识产权快速维权中心" },
+        { label: "知识产权维权援助中心" },
+      ],
+      open: false,
+    },
+    {
+        label: "全球维权援助网络1",
+        children: [
+        ],
+        open: false,
+      }
+  ]
   return (
     <div className="container mx-auto">
       <div className="py-3">
@@ -10,7 +37,7 @@ export default function RightsFlow() {
           BreadcrumbList={[{ label: "维权流程", href: "/rightsFlow" }]}
         ></BreadcrumbPlus>
       </div>
-        <SideBar></SideBar>
+        <SideBar sideBar={sideBarList}></SideBar>
   
   
     </div>
