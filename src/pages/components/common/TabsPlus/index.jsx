6a8120708f4186f7c0f83225985d704691cbfc3d
Isaac-chip <15622125865@163.com>
2023-04-18 16:57:53 +0800

    fix

--- src/pages/components/common/TabsPlus/index.jsx
+++ src/pages/components/common/TabsPlus/index.jsx
@@ -1,47 +1,28 @@
-import 'core-js/features/array/find-index'
+import { useState } from "react";
+import classNames from "classnames";
 
-import { Tabs, Pagination } from "flowbite-react";
-
-export default function TabsPlus({ tabs, activeTab = 0, onTabChange }) {
+export default function TabsPlus({ tabs, activeTab = 0 }) {
+  const [activeIndex, setActiveIndex] = useState(activeTab); 
   return (
     <>
-  
-      <Tabs.Group
-        onActiveTabChange={onTabChange}
-        aria-selected={" bg-pink-950"}
-        aria-label="Tabs with underline"
-        style="underline"
-        theme={{
-          base: "w-full",
-          tablist: {
-            tabitem: {
-              base: "flex items-center justify-center p-4 text-xl text-black font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500",
-              styles: {
-                default: {
-                  base: "rounded-t-lg",
-                },
-                underline: {
-                  base: "rounded-t-lg",
-                  active: {
-                    on: "text-black rounded-t-lg border-b-4 border-blue active dark:text-blue-500 dark:border-blue-500",
-                    off: "border-b-2 border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300",
-                  },
-                },
-              },
-            },
-          },
-        }}
-      >
-      {tabs?.map((tab, index) => (
-          <Tabs.Item active={activeTab == index} title={tab.label} key={index}>
-            {tab.content}
-          </Tabs.Item>
-        ))} 
-       
-      </Tabs.Group>
-      
-      
-    
+      <div className="w-full">
+        <div className=" mx-auto">
+          <div className="flex items-center -mb-px flex-grow border-b border-gray-200 ">
+           
+            {
+              tabs?.map((tab,index)=>{
+                return (
+                  <div onClick={()=>setActiveIndex(index)} className={classNames("py-4 font-bold text-xl mr-20 cursor-pointer  ",{"text-black border-b-4 border-blue":activeIndex===index},{"text-gray-400":activeIndex!==index})}>{tab.label}</div>
+                )
+              })
+            }
+          </div>
+        </div>
+        {/* content */}
+        <div className="mt-4">
+          {tabs && tabs[activeIndex]?.content}
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/components/license/index.jsx
+++ src/pages/components/license/index.jsx
@@ -1,11 +1,13 @@
 
 
+import { useState } from "react";
 import TabsPlus from "../common/TabsPlus";
 import PatentContent from "./patentContent";
 import TradeMark from "./tradeMark";
 import Copyright from "./copyright";
 
 export default function License() {
+
   const tabs = [
     {
       label: "专利",
@@ -30,8 +32,8 @@ export default function License() {
 
   return (
     <div className="container mx-auto">
-      <div className="flex relative ">
-        <TabsPlus activeTab={0} className="flex-shrink-0" tabs={tabs}></TabsPlus>
+      <div className="flex relative w-full">
+        <TabsPlus className="flex-shrink-0" tabs={tabs} onTabChange={(e)=>setActiveIndex(e)}></TabsPlus>
         <div className="absolute right-0 top-1 w-106px h-48px rounded-full bg-blue flex justify-center items-center text-white text-xl font-bold">
           发布
         </div>
