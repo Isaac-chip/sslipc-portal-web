97dec0196fccc13172392046f5ab524f295d777b
Isaac-chip <15622125865@163.com>
2023-04-18 15:10:42 +0800

    fix

--- src/pages/components/home/patentProject/desItem.jsx
+++ src/pages/components/home/patentProject/desItem.jsx
@@ -13,15 +13,15 @@ export default function DescItem({ isShowMask,data={光电领域: ["专利类型
         //   "linear-gradient(270deg, #FFFFFF 0%, rgba(217,217,217,0) 85%)",
       }}
     >
-      {Object.keys(data)?.map((item) => {
+      {Object.keys(data)?.map((item,index) => {
         return (
-          <div className="ml-20">
+          <div key={index} className="ml-20">
             <div className="font-bold text-xl pt-32px text-white">{item}</div>
             <div className="pt-10px">
               <div className="bg-blak flex">
-                {data[item]?.map((subItem, index) => {
+                {data[item]?.map((subItem, subIndex) => {
                   return (
-                    <div className="flex text-xs items-center text-gray-200">
+                    <div key={subIndex} className="flex text-xs items-center text-gray-200">
                       <div>{subItem}</div>
                       <div
                         className={classNames(
