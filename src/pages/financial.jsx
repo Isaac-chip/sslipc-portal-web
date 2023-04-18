4c73251a06b1ed611989c1d920ab1b55370a9d24
Isaac-chip <15622125865@163.com>
2023-04-18 14:35:21 +0800

    del file

--- src/pages/financial.jsx
+++
@@ -1,9 +0,0 @@
-// import Financial from "@/pages/components/Financial/index"
-
-export default function financialPage(){
-    return (
-        <div style={{background:"#FFFFFF"}}>
-           {/* <Financial></Financial>  */}
-        </div>
-    )
-}
