c27ceb2fc1eaa1951b95a3315bcc66db95ac84e8
Isaac-chip <15622125865@163.com>
2023-04-18 14:35:12 +0800

    modify page name

---
+++ src/pages/financialPage.jsx
@@ -0,0 +1,9 @@
+// import Financial from "@/pages/components/Financial/index"
+
+export default function financialPage(){
+    return (
+        <div style={{background:"#FFFFFF"}}>
+           {/* <Financial></Financial>  */}
+        </div>
+    )
+}
