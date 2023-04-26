d50b042a5c05eb7e165b0be147ca1f381362eb73
Isaac-chip <15622125865@163.com>
2023-04-26 15:57:18 +0800

    优化

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const fakeData = {
     首页: [],
-    IP大数据中心: [{label:'战略新兴产业专题数据库库',router:"/dataLibrary",target:"_blank"}, {label:'大数据展示分析',router:"/expert"}],
-    知识产权检索: [{label:'研发创新检索',router:"/expert",target:"_blank"},],
+    IP大数据中心: [{label:'战略新兴产业专题数据库库',router:"/dataLibrary",target:"_blank"}, {label:'大数据展示分析',router:"https://workspace.easyv.cloud/shareScreen/eyJzY3JlZW5JZCI6MTc3MTE0MX0=?timeStamp=1872631e044"}],
+    知识产权检索: [{label:'研发创新检索',router:"https://eureka.zhihuiya.com/",target:"_blank"},],
     交易金融中心: [{label:'项目专区',router:"/projectArea"}, {label:'开放许可',router:"/license"}, {label:'专利价值评估',router:"/expert"}, {label:'知识产权质押融资',router:"/financialPage"}],
     知识产权保护: [{label:'维权流程',router:"/rightsFlow"}, {label:'FTO',router:"/fto"}, {label:'海外专利预警',router:"/patentAlerts"}, {label:'维权案件库',router:"/caseLibrary"}],
   
@@ -9,7 +9,7 @@ export const fakeData = {
       
       // {label:'项目公告',router:"/expert"}, 
       {label:'专家库',target:"_bank",router:"/expert"}, {label:'通知公告',router:"/publicService",target:"_bank"}, {label:'政策信息',router:"/policyInfo"}],
-    教育培训: [{label:'公益课堂',router:"/expert"}, ],
+    教育培训: [{label:'公益课堂',router:"http://129.211.213.178:8090/#/training-course/index"}, ],
   };
   
   
--- src/pages/dataLibrary.jsx
+++ src/pages/dataLibrary.jsx
@@ -5,7 +5,7 @@ export default function dataLibrary(){
     return (
         <>
             <img className="w-570px h-84px mx-auto mt-32px mb-60px" src="/dataLibrary/zhuanjia.png" alt="" srcset="" />
-            <div className="grid container grid-cols-3 mx-auto">
+            <div className="grid container max-w-5xl grid-cols-3 mx-auto">
                 <img className="w-300px h-180px" src="/dataLibrary/bandaoti.png" alt="" srcset="" />
                 <img className="w-300px h-180px" src="/dataLibrary/ai.png" alt="" srcset="" />
                 <img className="w-300px h-180px" src="/dataLibrary/5g.png" alt="" srcset="" />
