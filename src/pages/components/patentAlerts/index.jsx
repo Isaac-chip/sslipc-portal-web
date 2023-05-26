eee494b16fbaf9e8cb09ae604fa8ec26492d8c2b
Isaac-chip <15622125865@163.com>
2023-05-26 14:19:13 +0800

    ui

--- src/pages/components/patentAlerts/index.jsx
+++ src/pages/components/patentAlerts/index.jsx
@@ -1,6 +1,4 @@
 import AlertSteps from "./alertSteps";
-
-import AlertSteps from "./alertSteps";
 export default function PatentAlerts() {
   return (
     <div className="flex mb-4 justify-center">
