d942b014dc6ab5ccc3aa43a0c480f0dfefa0d39f
Isaac-chip <15622125865@163.com>
2023-06-19 17:22:32 +0800

    优化

--- src/pages/components/common/Input/Input.jsx
+++ src/pages/components/common/Input/Input.jsx
@@ -1,11 +1,18 @@
-
-
-export default function Input({ type, placeholder, icon="/input/zhanghao.png" }) {
+export default function Input({
+  type,
+  placeholder,
+  icon = "/input/zhanghao.png",
+}) {
   return (
     <div className="relative flex items-center">
-      <img className="absolute ml-2  w-25px h-25px items-center" src={icon} alt="" srcset="" />
+      <img
+        className="absolute ml-2  w-25px h-25px items-center"
+        src={icon}
+        alt=""
+        srcset=""
+      />
       <input
-      style={{background:"#F2F6FA",borderColor:"#E7EFF8"}}
+        style={{ background: "#F2F6FA", borderColor: "#E7EFF8" }}
         type={type}
         placeholder={placeholder}
         className="pl-46px py-3 border w-full   rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
